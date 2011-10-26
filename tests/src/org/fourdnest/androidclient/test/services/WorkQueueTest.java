58ceeff1e700c75456b8b19d9e0a82bfda65da72
Stig-Arne Grönroos <sgronroo@cc.hut.fi>
2011-10-26 16:50:15 +0300

    Added some javadoc and comments.

--- tests/src/org/fourdnest/androidclient/test/services/WorkQueueTest.java
+++ tests/src/org/fourdnest/androidclient/test/services/WorkQueueTest.java
@@ -6,6 +6,11 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+/**
+ * Tests the work queue of the WorkerThread, using the dummy service
+ * @see TestService.
+ * @author gronsti
+ */
 public class WorkQueueTest {
 	private TestService testService;
 
@@ -13,6 +18,8 @@ public class WorkQueueTest {
 	public void setUp() throws Exception {
 		this.testService = new TestService();
 		this.testService.scheduleWork();
+		/* We need to sleep before running tests to make sure that the thread
+		 * has time to see the work and run it. */
 		boolean slept = false;
 		try {
 			while(!slept) {
