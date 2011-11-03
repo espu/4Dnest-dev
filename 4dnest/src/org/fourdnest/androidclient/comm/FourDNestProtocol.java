6aa59baccf069843af49aef7628970018318af99
Toni Mäki <maki.toni@gmail.com>
2011-11-03 16:28:20 +0200

    Adding response text to multientity post.

--- 4dnest/src/org/fourdnest/androidclient/comm/FourDNestProtocol.java
+++ 4dnest/src/org/fourdnest/androidclient/comm/FourDNestProtocol.java
@@ -6,8 +6,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.ResponseHandler;
 import org.fourdnest.androidclient.Egg;
 import org.fourdnest.androidclient.Tag;
+import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.protocol.BasicHttpContext;
@@ -49,17 +51,18 @@ public class FourDNestProtocol implements Protocol {
 		pairs.add(new BasicNameValuePair("name", "comma"));
 		pairs.add(new BasicNameValuePair("title", "Post_test"));
 		pairs.add(new BasicNameValuePair("caption", "Just testing with this funny picture"));
-		pairs.add(new BasicNameValuePair("image", "/sdcard/kuva.jpg"));
+		pairs.add(new BasicNameValuePair("file", "/sdcard/kuva.jpg"));
 		MultipartEntity entity = new MultipartEntity(HttpMultipartMode.STRICT);
 		for (int i = 0; i < pairs.size(); i ++) {
-			if (pairs.get(i).getName().equalsIgnoreCase("image")) {
+			if (pairs.get(i).getName().equalsIgnoreCase("file")) {
 				entity.addPart(pairs.get(i).getName(), new FileBody(new File(pairs.get(i).getValue())));
 			}else {
 				entity.addPart(pairs.get(i).getName(), new StringBody(pairs.get(i).getValue()));
 			}
 		}
 		post.setEntity(entity);
-		HttpResponse resp = client.execute(post);
-		return resp.toString();
+		ResponseHandler<String> responseHandler=new BasicResponseHandler();
+		String resp = client.execute(post, responseHandler);
+		return resp;
 	}
 }
