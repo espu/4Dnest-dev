f5dca775870a35ca53cce0a393fa0625e2c4605d
Toni Mäki <maki.toni@gmail.com>
2011-11-17 11:26:34 +0200

    Protocol Interface ready to be merged.

---
+++ 4dnest/src/org/fourdnest/androidclient/comm/Protocol.java
@@ -0,0 +1,15 @@
+package org.fourdnest.androidclient.comm;
+
+import org.fourdnest.androidclient.Egg;
+import org.fourdnest.androidclient.Nest;
+import org.fourdnest.androidclient.Tag;
+
+import java.util.ArrayList;
+
+
+public interface Protocol {
+    
+    public String sendEgg(Egg egg);
+    public ArrayList<Tag> topTags(int count);
+    public void setNest(Nest nest);
+}
