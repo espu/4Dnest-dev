c8e75e057ac585340c7dc122468cc1b7774bb399
Matias Kuusela <jmkuuse2@cc.hut.fi>
2011-11-17 17:16:12 +0200

    Changes to browsing pictures and how to proceed from there! Fun Stuff!

--- 4dnest/src/org/fourdnest/androidclient/ui/MainScreenFragment.java
+++ 4dnest/src/org/fourdnest/androidclient/ui/MainScreenFragment.java
@@ -2,28 +2,43 @@ package org.fourdnest.androidclient.ui;
 
 import org.fourdnest.androidclient.R;
 
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.CursorLoader;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+/*
+ * Items related to using media gallery, credits go to szhln from stackoverflow
+ * Original post: http://stackoverflow.com/questions/2169649/open-an-image-in-androids-built-in-gallery-app-programmatically
+ */
+
+
 public class MainScreenFragment extends Fragment {
 	
 	//YOU CAN EDIT THIS TO WHATEVER YOU WANT
     private static final int SELECT_PICTURE = 1;
+    private static final int RESULT_OK = 1;
+
 
     private String selectedImagePath;
     //ADDED
     private String filemanagerstring;
+    private Context currentContext;
 	
 	
 	
-	public MainScreenFragment() {
+	public MainScreenFragment(Context currentContext) {
 		super();
+		this.currentContext = currentContext;
 	}
 	
 	@Override
@@ -43,7 +58,6 @@ public class MainScreenFragment extends Fragment {
         .setOnClickListener(new OnClickListener() {
 
             public void onClick(View arg0) {
-
                 // in onCreate or any event where your want the user to
                 // select a file
                 Intent intent = new Intent();
@@ -55,4 +69,56 @@ public class MainScreenFragment extends Fragment {
         });
 		return view;
 	}
+	
+    //UPDATED
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            if (requestCode == SELECT_PICTURE) {
+                Uri selectedImageUri = data.getData();
+
+                //OI FILE Manager
+                filemanagerstring = selectedImageUri.getPath();
+
+                //MEDIA GALLERY
+                selectedImagePath = getPath(selectedImageUri);
+
+                //DEBUG PURPOSE - you can delete this if you want
+                if(selectedImagePath!=null)
+                    System.out.println(selectedImagePath);
+                else System.out.println("selectedImagePath is null");
+                if(filemanagerstring!=null)
+                    System.out.println(filemanagerstring);
+                else System.out.println("filemanagerstring is null");
+
+                //NOW WE HAVE OUR WANTED STRING
+                if(selectedImagePath!=null)
+                    System.out.println("selectedImagePath is the right one for you!");
+                else
+                    System.out.println("filemanagerstring is the right one for you!");
+            }
+        }
+    }
+
+    //UPDATED!
+    public String getPath(Uri uri) {
+        String[] projection = { MediaStore.Images.Media.DATA };
+
+        CursorLoader loader = new CursorLoader(this.currentContext, uri, projection, null, null, null);
+        Cursor cursor = null;
+        if(cursor!=null)
+        {
+            //HERE YOU WILL GET A NULLPOINTER IF CURSOR IS NULL
+            //THIS CAN BE, IF YOU USED OI FILE MANAGER FOR PICKING THE MEDIA
+            int column_index = cursor
+            .getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            cursor.moveToFirst();
+            return cursor.getString(column_index);
+        }
+        else return null;
+    }
+	
+	
+	
+	
+	
 }
