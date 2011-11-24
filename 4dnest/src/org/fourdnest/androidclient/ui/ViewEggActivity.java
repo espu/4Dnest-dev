435766c05d61839317cfe544521afda959304b53
Jizhou Yang <yakisoba666@gmail.com>
2011-11-24 16:52:44 +0200

    Added preliminaries for ViewEggActivity

---
+++ 4dnest/src/org/fourdnest/androidclient/ui/ViewEggActivity.java
@@ -0,0 +1,19 @@
+package org.fourdnest.androidclient.ui;
+
+import org.fourdnest.androidclient.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+
+public class ViewEggActivity extends Activity {
+	
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+	}
+
+}
