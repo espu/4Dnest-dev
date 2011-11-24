a7da556d49e828a1fed12012b23fd3c271175652
Jizhou Yang <yakisoba666@gmail.com>
2011-11-24 18:14:06 +0200

    Removed commented code.

--- 4dnest/src/org/fourdnest/androidclient/ui/EggReaderAdapter.java
+++ 4dnest/src/org/fourdnest/androidclient/ui/EggReaderAdapter.java
@@ -39,18 +39,6 @@ public class EggReaderAdapter extends EggListAdapter {
 		TextView message = (TextView) arg1.findViewById(R.id.message);
 		TextView date = (TextView) arg1.findViewById(R.id.date);
 
-		// There might be listeners for ListViews which don't require setting
-		// individual onClickListeners for each egg preview.
-//		arg1.setOnClickListener(new OnClickListener() {
-//
-//			public void onClick(View v) {
-//				Intent intent = new Intent(v.getContext(),
-//						ViewEggActivity.class);
-//				parent.getContext().startActivity(intent);
-//
-//			}
-//		});
-
 		return arg1;
 	}
 
