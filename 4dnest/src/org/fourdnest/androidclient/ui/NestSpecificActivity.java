3bf964f5ee23b9696eb08acfa4c4f9bd545fa4d8
Jizhou Yang <yakisoba666@gmail.com>
2011-12-01 18:16:18 +0200

    Working implementation of NestSpecificActivity and some of its implementing classes.

--- 4dnest/src/org/fourdnest/androidclient/ui/NestSpecificActivity.java
+++ 4dnest/src/org/fourdnest/androidclient/ui/NestSpecificActivity.java
@@ -4,16 +4,35 @@ import org.fourdnest.androidclient.R;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
 
 /**
  * Activity for displaying the current Nest at the top of the screen.
  */
-public class NestSpecificActivity extends Activity {
+public abstract class NestSpecificActivity extends Activity {
 
+	protected ViewGroup contentView;
+	
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
+		setContentView(R.layout.main_view);
+		
+		this.contentView = (FrameLayout)findViewById(R.id.content_view);
+		
+		LayoutInflater inflater = LayoutInflater.from(contentView.getContext());
+		View view = inflater.inflate(getLayoutId(), contentView, false);
+		contentView.addView(getContentLayout(view));
 	}
+	
+	/**
+	 * Sets the main content view. The main content view is the entire area under the Nest display.
+	 * @param view The ViewGroup where you want to display your functional content.
+	 */
+	public abstract View getContentLayout(View view);
+	public abstract int getLayoutId();
 }
