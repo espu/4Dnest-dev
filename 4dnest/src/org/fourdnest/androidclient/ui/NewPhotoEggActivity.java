5a6d9f404aeb608e9f68958d9358a9c16eac37bd
Jizhou Yang <yakisoba666@gmail.com>
2011-12-01 13:52:25 +0200

    Servicing. Yay!

--- 4dnest/src/org/fourdnest/androidclient/ui/NewPhotoEggActivity.java
+++ 4dnest/src/org/fourdnest/androidclient/ui/NewPhotoEggActivity.java
@@ -14,6 +14,7 @@ import android.support.v4.view.ViewPager;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.webkit.MimeTypeMap;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class NewPhotoEggActivity extends Activity{
@@ -58,7 +59,13 @@ public class NewPhotoEggActivity extends Activity{
             }
         });
 	
-        
+        Button sendButton = (Button) findViewById(R.id.new_photo_egg_send_egg_button);
+        sendButton.setOnClickListener(new OnClickListener() {
+			
+			public void onClick(View v) {
+				//TODO: Add Egg to queue
+			}
+		});
         
         
 
--- 4dnest/src/org/fourdnest/androidclient/ui/ReadStreamActivity.java
+++ 4dnest/src/org/fourdnest/androidclient/ui/ReadStreamActivity.java
@@ -1,24 +1,35 @@
 package org.fourdnest.androidclient.ui;
 
 import org.fourdnest.androidclient.R;
+import org.fourdnest.androidclient.services.SendQueueService;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ListView;
 
+/**
+ * The starting activity of the application. Displays a list of eggs present on
+ * the server. Also provides functionality to access the create view and for
+ * toggling route tracking.
+ */
 public class ReadStreamActivity extends Activity {
-	
+
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.read_stream_view);
-		
+
 		ListView streamList = (ListView) findViewById(R.id.egg_list);
 		EggReaderAdapter adapter = new EggReaderAdapter(streamList);
 		adapter.setEggs(null);
 		streamList.setAdapter(adapter);
 		streamList.setOnItemClickListener(new EggElementOnClickListener());
+		
+		//TODO: Put this somewhere else
+		Intent startServiceIntent = new Intent(this.getApplicationContext(), SendQueueService.class);
+		this.getApplicationContext().startService(startServiceIntent);
 	}
 
 }
