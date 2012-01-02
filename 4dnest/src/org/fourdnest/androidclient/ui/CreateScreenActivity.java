f785bed21743ea4d8225fa86ac618df33cc4fa91
Jizhou Yang <yakisoba666@gmail.com>
2012-01-02 16:19:05 +0200

    Removed unused imports.

--- 4dnest/src/org/fourdnest/androidclient/ui/CreateScreenActivity.java
+++ 4dnest/src/org/fourdnest/androidclient/ui/CreateScreenActivity.java
@@ -5,7 +5,6 @@ import org.fourdnest.androidclient.R;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
-import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v4.content.CursorLoader;
 import android.view.View;
--- 4dnest/src/org/fourdnest/androidclient/ui/ListStreamFragment.java
+++ 4dnest/src/org/fourdnest/androidclient/ui/ListStreamFragment.java
@@ -2,14 +2,11 @@ package org.fourdnest.androidclient.ui;
 
 import org.fourdnest.androidclient.R;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
 
 /**
