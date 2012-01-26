a6535eaf394d333d15f60bc95dfe78bf6c86c85d
Toni Mäki <maki.toni@gmail.com>
2012-01-26 13:37:45 +0200

    Switched FourDnestApplication to the one in the master branch.

--- 4dnest/src/org/fourdnest/androidclient/FourDNestApplication.java
+++ 4dnest/src/org/fourdnest/androidclient/FourDNestApplication.java
@@ -2,6 +2,8 @@ package org.fourdnest.androidclient;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.List;
+
 import org.fourdnest.androidclient.comm.ProtocolFactory;
 import org.fourdnest.androidclient.comm.UnknownProtocolException;
 import org.fourdnest.androidclient.services.StreamReaderService;
@@ -11,6 +13,7 @@ import android.app.Application;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
+import android.os.Handler;
 import android.preference.PreferenceManager;
 import android.util.Log;
 import android.widget.Toast;
@@ -23,181 +26,240 @@ import android.widget.Toast;
  * FourDNestApplication application = (FourDNestApplication) getApplication() 
  */
 public class FourDNestApplication extends Application
-	implements OnSharedPreferenceChangeListener {
-	private static final String TAG = FourDNestApplication.class.getSimpleName();
-	
-	private SharedPreferences prefs;
-	private NestManager nestManager;
-	
-	private static final String draftEggManagerRole = "draft";
-	private EggManager draftEggManager;
+    implements OnSharedPreferenceChangeListener {
+    private static final String TAG = FourDNestApplication.class.getSimpleName();
+    private static final int NEST_ID = 1;
+    
+    private SharedPreferences prefs;
+    private NestManager nestManager;
+    
+    private static final String draftEggManagerRole = "draft";
+    private EggManager draftEggManager;
+    private static final String streamEggManagerRole = "stream";
+    private EggManager streamEggManager;
+    
+    private Handler handler;
+    
+    private static FourDNestApplication app;
+    
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        synchronized(this) {
+            this.prefs = PreferenceManager.getDefaultSharedPreferences(this);
+            this.prefs.registerOnSharedPreferenceChangeListener(this);    
+            // For debugging, insert default Nest and settings
+            Nest nest = this.getNestManager().getNest(NEST_ID);
+            if(nest == null) {
+                Nest defaultNest = this.getDefaultNest();
+                this.getNestManager().saveNest(defaultNest);
+                this.setCurrentNestId(defaultNest.getId());
 
-	private static final String streamEggManagerRole = "stream";
-	private EggManager streamEggManager;
-	
-	private static FourDNestApplication app;
+                this.prefs.edit()
+                .putString("nest_base_uri", defaultNest.getBaseURI().toString())
+                .putString("nest_username", defaultNest.getUserName())
+                .putString("nest_password", defaultNest.getSecretKey())
+                .commit();
+            } else {
+                this.setCurrentNestId(nest.getId());
+            }
 
-	
-	@Override
-	public void onCreate() { //
-	  super.onCreate();
-	  this.prefs = PreferenceManager.getDefaultSharedPreferences(this);
-	  this.prefs.registerOnSharedPreferenceChangeListener(this);
+            // Init handler
+            this.handler = new Handler();
+      }
+      
+      app = this;
+      Log.i(TAG, "onCreated");
+      //warm start TagSuggestionService
+      startService(new Intent(this, StreamReaderService.class));
+      startService(new Intent(this, TagSuggestionService.class));
+    }
+    
+    public static FourDNestApplication getApplication() {
+        return app;
+    }
 
-	  this.setUpTestValues();
+    /**
+     * Gets the NestManager singleton.
+     * The NestManager object synchronizes itself, so the caller is free to store
+     * the object.  
+     * @return the NestManager
+     */
+    public NestManager getNestManager() {
+        if(this.nestManager == null) {
+            this.nestManager = new NestManager(this);
+        }
+        return this.nestManager;
+    }
+    
+    /**
+     * Gets the EggManager singleton for draft eggs.
+     * The EggManager object synchronizes itself, so the caller is free to store
+     * the object.
+     * @return the EggManager
+     */
+    public EggManager getDraftEggManager() {
+        if(this.draftEggManager == null) {
+            this.draftEggManager = new EggManager(this, this.draftEggManagerRole);
+        }
+        return this.draftEggManager;
+    }
+    
+    /**
+     * Gets the EggManager singleton for stream eggs.
+     * The EggManager object synchronizes itself, so the caller is free to store
+     * the object.
+     * @return the EggManager
+     */
+    public EggManager getStreamEggManager() {
+        if(this.streamEggManager == null) {
+            this.streamEggManager = new EggManager(this, this.streamEggManagerRole);
+        }
+        return this.streamEggManager;
+    }
+    
+    
+    /**
+     * Handler for OnSharedPreferenceChangeListener
+     * Called when preferences change
+     */
+    public synchronized void onSharedPreferenceChanged(
+            SharedPreferences sharedPreferences, String key) {
+        this.getApplicationContext();
+        
+        Log.d(TAG, "pref " + key + " changed");
 
-	  app = this;
+        // When a nest_ preference changes, call relevant handler
+        if(key.startsWith("nest_")) {
+            this.handleNestPrefChanges();
+        }
+    }
+    
+    /**
+     * Returns current active nest from nest manager
+     * @return currently active Nest
+     */
+    public synchronized Nest getCurrentNest() {
+        return this.nestManager.getNest(this.getCurrentNestId());
+    }
 
-	  Log.i(TAG, "onCreated");
-	  //warm start TagSuggestionService
-	  startService(new Intent(this, TagSuggestionService.class));
-	  startService(new Intent(this, StreamReaderService.class));
-	}
-	
-	public static FourDNestApplication getApplication() {
-		return app;
-	}
-	/**
-	 * Checks if Nest with ID 0 exists and creates it if not.
-	 * Temporary debug-helper method.
-	 */
-	private void setUpTestValues() {
-		try {
-			NestManager m = this.getNestManager();
-			Nest n = m.getNest(0);
-			
-			if(n == null) {
-				n = new Nest(0, "testNest", "testNest", new URI("http://test42.4dnest.org/"), ProtocolFactory.PROTOCOL_4DNEST, "testuser", "secretkey");
-				m.saveNest(n);
-			}
-			this.setCurrentNestId(n.getId());
-		} catch(URISyntaxException urie) {	
-		} catch(UnknownProtocolException upe) {
-		}
-	}
+    /**
+     * Gets the id of the currently active Nest.
+     * @return The id of the currently active Nest. 
+     */
+    public synchronized int getCurrentNestId() {
+        return this.prefs.getInt("currentNestId", 0);
+    }
+    
 
-	/**
-	 * Gets the NestManager singleton.
-	 * The NestManager object synchronizes itself, so the caller is free to store
-	 * the object.  
-	 * @return the NestManager
-	 */
-	public NestManager getNestManager() {
-		if(this.nestManager == null) {
-			this.nestManager = new NestManager(this);
-		}
-		return this.nestManager;
-	}
-	
-	/**
-	 * Gets the EggManager singleton for draft eggs.
-	 * The EggManager object synchronizes itself, so the caller is free to store
-	 * the object.
-	 * @return the EggManager
-	 */
-	public EggManager getDraftEggManager() {
-		if(this.draftEggManager == null) {
-			this.draftEggManager = new EggManager(this, this.draftEggManagerRole);
-		}
-		return this.draftEggManager;
-	}
-	
-	/**
-	 * Gets the EggManager singleton for stream eggs.
-	 * The EggManager object synchronizes itself, so the caller is free to store
-	 * the object.
-	 * @return the EggManager
-	 */
-	public EggManager getStreamEggManager() {
-		if(this.streamEggManager == null) {
-			this.streamEggManager = new EggManager(this, this.streamEggManagerRole);
-		}
-		return this.streamEggManager;
-	}
-	
-	
+    /**
+     * Sets the currently active Nest. The setting is persisted between
+     * restarts of the application.
+     * @param newNestId Id of the new active Nest. Must be a valid Nest id.
+     */
+    public synchronized void setCurrentNestId(int newNestId) {
+        this.prefs.edit()
+            .putInt("currentNestId", newNestId)
+            .commit();
+    }
+    
+    /**
+     * Called when onSharedPreferenceChanged detects nest_ -prefixed
+     * preference change.
+     * 
+     * Creates new active nest with extracted URL, user name and password
+     */
+    private synchronized void handleNestPrefChanges() {
+        String baseUri = this.prefs.getString("nest_base_uri", "");
+        String user = this.prefs.getString("nest_username", "");
+        String pass = this.prefs.getString("nest_password", "");
+        
+        URI newURI = null;
+        URI defaultURI = null;
+        try {
+            newURI = new URI(baseUri);
+            new URI("");
+        } catch(URISyntaxException e) {
+            this.handler.post(new ToastDisplay("Invalid URI setting", Toast.LENGTH_SHORT));
+            newURI = defaultURI;
+        }
+        NestManager m = this.getNestManager();
+        m.deleteAllNests();
 
-
-	public synchronized void onSharedPreferenceChanged(
-			SharedPreferences sharedPreferences, String key) {
-		this.getApplicationContext();
-		
-		Log.d(TAG, "pref " + key + " changed");
-		// Dummy implementation: When nest base URI pref changes,
-		// drop all existing nests and create a new Fourdnest with
-		// the base uri
-		if(key.equals("nest_base_uri")) {
-			String newVal = this.prefs.getString("nest_base_uri", "");
-			
-			URI newURI = null;
-			URI defaultURI = null;
-			try {
-				newURI = new URI(newVal);
-				new URI("");
-			} catch(URISyntaxException e) {
-				Toast.makeText(this, "Invalid URI, using default", Toast.LENGTH_SHORT);
-				newURI = defaultURI;
-			}
-			NestManager m = this.getNestManager();
-			m.deleteAllNests();
-
-			Nest n = null;
-			try {
-				n = new Nest(0, "testNest", "testNest", newURI, ProtocolFactory.PROTOCOL_4DNEST, "testuser", "secretkey");
-				m.saveNest(n);
-				this.setCurrentNestId(n.getId());
-			} catch(UnknownProtocolException upe) {	}
-
-		}
-	}
-	
-	public synchronized Nest getCurrentNest() {
-		return this.nestManager.getNest(this.getCurrentNestId());
-	}
-
-	/**
-	 * Gets the id of the currently active Nest.
-	 * @return The id of the currently active Nest. 
-	 */
-	public synchronized int getCurrentNestId() {
-		return this.prefs.getInt("currentNestId", 0);
-	}
-	
-
-	/**
-	 * Sets the currently active Nest. The setting is persisted between
-	 * restarts of the application.
-	 * @param newNestId Id of the new active Nest. Must be a valid Nest id.
-	 */
-	public synchronized void setCurrentNestId(int newNestId) {
-		//FIXME check that newNestId is valid?
-		SharedPreferences.Editor prefEditor = this.prefs.edit();
-		prefEditor.putInt("currentNestId", newNestId);
-	}
-	
-	/**
-	 * Are all certificates valid without checking?
-	 * @return boolean setting value, default true
-	 */
-	public synchronized boolean getAllowAllCerts() {
-		return this.prefs.getBoolean("allow_all_certs", true);
-	}
-	
-	/**
-	 * Is kiosk mode enabled?
-	 * @return boolean setting value, default true
-	 */
-	public synchronized boolean getKioskModeEnabled() {		
-		return this.prefs.getBoolean("kiosk_mode", false);
-	}
-	
-	/**
-	 * Set value of allow_all_certs setting. Used in testing.
-	 * @param val
-	 */
-	public synchronized void setAllowAllCerts(Boolean val) {
-		SharedPreferences.Editor prefEditor = this.prefs.edit();
-		prefEditor.putBoolean("allow_all_certs", val);
-	}
+        Nest n = null;
+        try {
+            n = new Nest(NEST_ID, "Active Nest", "Active Nest", newURI, ProtocolFactory.PROTOCOL_4DNEST, user, pass);
+            m.saveNest(n);
+            this.setCurrentNestId(n.getId());
+        } catch(UnknownProtocolException upe) { }
+    }
+    
+    /**
+     * Returns a Nest with default dev server properties. Debug use.
+     */
+    private Nest getDefaultNest() {
+        Nest n = null;
+        try {
+            n = new Nest(NEST_ID, "testNest", "testNest", new URI("http://test42.4dnest.org/"), ProtocolFactory.PROTOCOL_4DNEST, "testuser", "secretkey");
+        } catch(URISyntaxException urie) {  
+        } catch(UnknownProtocolException upe) {
+        }
+        
+        return n;
+    }
+    
+    /**
+     * Internal class for showing messages from background threads (e.g. pref changes)
+     */
+    private class ToastDisplay implements Runnable {
+        private String message;
+        private int duration;
+        
+        /**
+         * Runnable ToastDisplayer, added to a Handler to be run at a later time or
+         * in a different thread
+         * @param context Target context to display Toast in
+         * @param message Message content
+         * @param duration Toast.LENGTH_SHORT or Toast.LENGTH_LONG
+         */
+        public ToastDisplay(String message, int duration) {
+            this.message = message;
+            this.duration = duration;
+        }
+        
+        /**
+         * Called when Runnable is executed. displays Toast as specified when object was created
+         */
+        public void run() {
+            Toast.makeText(app, this.message, this.duration).show();
+        }
+    
+    }
+    
+    /**
+     * Are all certificates valid without checking?
+     * @return boolean setting value, default true
+     */
+    public synchronized boolean getAllowAllCerts() {
+        return this.prefs.getBoolean("nest_accept_all_certs", true);
+    }
+    
+    /**
+     * Is kiosk mode enabled?
+     * @return boolean setting value, default true
+     */
+    public synchronized boolean getKioskModeEnabled() {     
+        return this.prefs.getBoolean("kiosk_mode", false);
+    }
+    
+    /**
+     * Set value of allow_all_certs setting. Used in testing.
+     * @param val
+     */
+    public synchronized void setAllowAllCerts(Boolean val) {
+        SharedPreferences.Editor prefEditor = this.prefs.edit();
+        prefEditor.putBoolean("nest_accept_all_certs", val);
+    }
+    
 
 }
