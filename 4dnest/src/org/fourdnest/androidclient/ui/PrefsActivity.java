8e41f9d53d0779483b34db2a3b805a598925b3fb
Sami Huoman <sami.huoman@aalto.fi>
2012-01-26 15:29:18 +0200

    Added comments to PrefsActivity

--- 4dnest/src/org/fourdnest/androidclient/ui/PrefsActivity.java
+++ 4dnest/src/org/fourdnest/androidclient/ui/PrefsActivity.java
@@ -5,7 +5,15 @@ import org.fourdnest.androidclient.R;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 
+/**
+ * The preference activity for 4DNest that handles all the preference specific activity.
+ */
 public class PrefsActivity extends PreferenceActivity {
+    /**
+     * Determines what happens when the PreferenceActivity is created.
+     *
+     * @param savedInstanceState Contains information regarding the state of the activity
+     */
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 	super.onCreate(savedInstanceState);
