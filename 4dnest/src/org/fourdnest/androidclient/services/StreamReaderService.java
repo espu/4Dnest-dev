4a5744bd6d569e5e8592ae95a6c830844c67edc5
Matias Kuusela <jmkuuse2@cc.hut.fi>
2012-02-02 15:15:34 +0200

    Strange thinks

--- 4dnest/src/org/fourdnest/androidclient/services/StreamReaderService.java
+++ 4dnest/src/org/fourdnest/androidclient/services/StreamReaderService.java
@@ -15,7 +15,9 @@ import android.app.Application;
 import android.app.IntentService;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Environment;
+import android.preference.PreferenceManager;
 import android.util.Log;
 
 public class StreamReaderService extends IntentService {
@@ -24,6 +26,10 @@ public class StreamReaderService extends IntentService {
     
     /**Intent should have this category when stream should be read from the server */
     public static final String READ_STREAM = "READ_STREAM_CATEGORY";
+    public static final String STREAM_FREQ = "STREAM_FREQUENCY_CATEGORY";
+    public static final String NEW_FREQUENCY = "NEW_FRQUENCY";
+    public static final String STREAM_SIZE = "STREAM_SIZE_CATEGORY";
+    public static final String NEW_STREAM_SIZE = "NEW_STREAM_SIZE";
     
     /**How long we initially wait before fetching the Stream*/
     public static final long FIRST_INTERVAL = 0;
@@ -36,9 +42,14 @@ public class StreamReaderService extends IntentService {
     
 
     
-    private static final long TWO_MINUTE = 120000;
+    /** Default Frequency in seconds, as a String so it can be used in the getPreference method*/
+    private static final String DEFAULT_FREQUENCY = "600";
+    
+    /** Default Frequency in seconds, as a String so it can be used in the getPreference method*/
+    private static final String DEFAULT_SIZE = "10";
     
     private FourDNestApplication app;
+    private int size = Integer.parseInt(DEFAULT_SIZE);
     
     public StreamReaderService() {
         super(StreamReaderService.class.getName());
@@ -54,13 +65,20 @@ public class StreamReaderService extends IntentService {
     	Log.d(TAG, "Intentcreated");
         
         app = FourDNestApplication.getApplication();
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(app);
+        int frequency = Integer.parseInt(prefs.getString("stream_frequency",
+               String.valueOf(DEFAULT_FREQUENCY))) * 60000;
+        size = Integer.parseInt(prefs.getString("stream_size", String
+                .valueOf(DEFAULT_SIZE)));
+        Log.d(TAG, "Value of freq after read: " + frequency);
+        Log.d(TAG, "Value of size after read: " + size);
         AlarmManager am = (AlarmManager) getSystemService(ALARM_SERVICE);
         Intent intent = new Intent(app, StreamReaderService.class);
         intent.addCategory(READ_STREAM);
         am.setInexactRepeating( //Be power efficient: we don't need exact timing
                 AlarmManager.ELAPSED_REALTIME,  // Don't wake up phone just for this
-                FIRST_INTERVAL,                             
-                TWO_MINUTE,     // Update frequency
+                FIRST_INTERVAL,      //Don't run on start                            
+                frequency,     // Update frequency
                 PendingIntent.getService(
                         app,                    // The context
                         0,
@@ -79,12 +97,15 @@ public class StreamReaderService extends IntentService {
     protected void onHandleIntent(Intent intent) {
     	Log.d(TAG, "Handling intent");
         if (intent.hasCategory(READ_STREAM)) {
+            Log.d(TAG, "Got READ_STREAM category");
             EggManager em = app.getStreamEggManager();
-            List<Egg> eggList = app.getCurrentNest().getProtocol().getStream();
+            em.deleteAllEggs();
+            List<Egg> eggList = app.getCurrentNest().getProtocol().getStream(size);
             Log.d(TAG, "Egglist size: " + eggList.size());
             em.deleteAllEggs();
             ThumbnailManager thumbnailManager = new FourDNestThumbnailManager();
             for (int i = 0; i < eggList.size(); i++) {
+
             	Egg egg = eggList.get(i);
                 em.saveEgg(egg);
                 thumbnailManager.getThumbnail(egg);
@@ -93,6 +114,9 @@ public class StreamReaderService extends IntentService {
             Log.d(TAG, "Saved eggs");
             List<Egg> eggs = em.listEggs();
             Log.d("EGGAMOUNT2", String.valueOf(eggs.size()));
+        }
+        if (intent.hasCategory(STREAM_FREQ)) {
+        	Log.d(TAG, "Got updated frequency");
         }
     }
 
